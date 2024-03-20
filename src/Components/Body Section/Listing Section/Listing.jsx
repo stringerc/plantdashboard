74e9362e5427293c45b7105ff4c33dff8cd45e38
stringerc <stringer.c.a@gmail.com>
2024-03-20 03:11:42 -0400

    First upload

---
+++ src/Components/Body Section/Listing Section/Listing.jsx
@@ -0,0 +1,93 @@
+import React from 'react'
+import './listing.css'
+import { BsArrowRightShort } from 'react-icons/bs'
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai'
+
+// Imported Images
+import img from '../../../Assets/image(1).png' 
+import img1 from '../../../Assets/image(9).png' 
+import img2 from '../../../Assets/image(8).png' 
+import img3 from '../../../Assets/image(10).png'
+import user1 from '../../../Assets/user (2).png'
+import user2 from '../../../Assets/user(3).jpeg'
+import user3 from '../../../Assets//user(4).jpeg'
+import user4 from '../../../Assets/user(5).jpeg'
+
+
+const Listing = () => {
+  return (
+    <div className='listingSection'>
+      <div className="heading flex">
+        <h1>My Listings</h1>
+        <button className="btn flex">
+            See All <BsArrowRightShort className='icon'/>
+        </button>
+      </div>
+
+      <div className="secContainer flex">
+        <div className="singleItem">
+            <AiFillHeart className='icon' />
+            <img src={img} alt="" />
+            <h3>Annual Vince</h3>
+        </div>
+      
+      
+        <div className="singleItem">
+            <AiFillHeart className='icon' />
+            <img src={img} alt="" />
+            <h3>Annual Vince</h3>
+        </div>
+
+        <div className="singleItem">
+            <AiFillHeart className='icon' />
+            <img src={img1} alt="" />
+            <h3>Coffee Plant</h3>
+        </div>
+
+        <div className="singleItem">
+            <AiOutlineHeart className='icon' />
+            <img src={img2} alt="" />
+            <h3>Button Fern</h3>
+        </div>
+
+        <div className="singleItem">
+            <AiFillHeart className='icon' />
+            <img src={img3} alt="" />
+            <h3>Spider Plant</h3>
+        </div>
+      
+    
+    
+      <div className="sellers flex">
+        <div className="topSellers"></div>
+          <div className="listing_heading flex">
+            <h1>Top Sellers</h1>
+            <button className="btn flex">
+            See All <BsArrowRightShort className='icon'/>
+            </button>
+          </div>
+
+          <div className="card flex">
+            <div className="users">
+              <img src={user1} alt="User Logo" />
+              <img src={user2} alt="User Logo" />
+              <img src={user3} alt="User Logo" />
+              <img src={user4} alt="User Logo" />
+            </div>
+            <div className="cardText">
+              <span>
+                14.566 Plants sold <br/>
+              
+              <small>
+                21 Sellers <span className="date">7 Days</span>
+              </small>
+              </span>
+            </div>
+          </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Listing
---
+++ src/Components/Body Section/Top Section/Top.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import './top.css'
+
+
+//Imported Icons ===============>
+import { BiSearchAlt } from 'react-icons/bi'
+import { TbMessageCircle } from 'react-icons/tb'
+import { MdOutlineNotificationsNone } from 'react-icons/md'
+import { BsArrowRightShort } from 'react-icons/bs'
+
+// Importerd Imaged ===================>
+import img from '../../../Assets/user (3).png'
+import img2 from '../../../Assets/image(2).png'
+import video from '../../../Assets/video.mov'
+
+
+const Top = () => {
+  return (
+    <div className='topSection'>
+      <div className="headerSection flex">
+        <div className="title">
+            <h1>Welcome to Pranti</h1>
+            <p>Hello Chris, Welcome back!</p>
+        </div>
+
+        <div className="searchBar flex">
+            <input type="text" placeholder='Search Dashbar' />
+            <BiSearchAlt className='icon'/>
+        </div>
+
+        <div className="adminDiv flex">
+            <TbMessageCircle className='icon' />
+            <MdOutlineNotificationsNone className='icon' />
+            <div className="adminImage">
+                <img src={img} alt="Admin Image" />
+            </div>
+        </div>
+
+        
+      </div>
+
+      <div className="cardSection flex">
+
+        <div className="rightCard flex">
+            <h1>Create and sell extraordinary products</h1>
+            <p>The world's fast growing industry today are natural made products</p>
+        
+
+        <div className="buttons flex">
+            <button className="btn">Explore More</button>
+            <button className="btn transparent">Top Sellers</button>
+        </div>
+        
+        <div className="videoDiv">
+            <video src={video} autoPlay loop muted></video>
+        </div>
+        </div>
+
+        <div className="leftCard flex">
+            <div className="main flex">
+
+                <div className="textDiv">
+                    <h1>My Stat</h1>
+
+                    <div className="flex">
+                        <span>
+                            Today <br/> <small>4 Orders</small>
+                        </span>
+                        <span>
+                            This Month <br/> <small>175 Orders</small>
+                        </span>
+                    </div>
+                    
+                    <span className="flex link">
+                        Go to my orders <BsArrowRightShort
+                        className='icon'/>
+                    </span>
+                </div>
+
+               <div className="imgDiv">
+                <img src={img2} alt="" />
+               </div>
+
+               {/* <div className="sideBarCard">
+                    <BsQuestionCircle className='icon'/>
+                    <div className="cardContent">
+                        <div className="circle1"></div>
+                        <div className="circle2"></div>
+
+                        <h3>Help Center</h3>
+                        <p>Having trouble in Planti, please contact us for more questions
+                        </p>
+                        <button className='btn'>Go to help center</button>
+                    </div>
+                </div>  */}
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Top
